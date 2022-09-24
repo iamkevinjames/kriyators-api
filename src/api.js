60a6d6a82a8cc70397d5858080b5348e510de162
iamkevinjames <75653990+iamkevinjames@users.noreply.github.com>
2022-09-24 12:49:50 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,1 +1,27 @@
+const express = require("express");
+const serverless = require("serverless-http");
 
+const app = express();
+const router = express.Router();
+
+const data = {
+  firstName: "Vladimir",
+  lastName: "Putin",
+  emailAddress: "vladmirputin@kyro.com",
+};
+
+router.get("/getData", (req, res) => {
+  res.send(data);
+});
+
+router.post("/postData", (req, res) => {
+  data.firstName = req.body.firstName;
+  data.lastName = req.body.lastName;
+  data.emailAddress = req.body.emailAddress;
+  res.send("User Data updated successfully!");
+});
+
+app.use(`/.netlify/functions/api`, router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
